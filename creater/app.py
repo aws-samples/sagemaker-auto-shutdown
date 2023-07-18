57c6324c2878a9a9d9806ffb5a1fc47b479776f0
Shing Lyu <shinglyu@amazon.nl>
2023-07-18 13:04:17 +0000

    feat: change the endpoint spec to be an array so we can deploy multiple endpoints

--- creater/app.py
+++ creater/app.py
@@ -14,21 +14,32 @@ def lambda_handler(event, context):
     if not endpoint_spec:
         logger.error("Environment variable 'ENDPOINT_SPEC' not set")
         raise Exception("Environment variable 'ENDPOINT_SPEC' not set")
-    if not ('EndpointConfigName' in endpoint_spec and 'EndpointName' in endpoint_spec):
-        logger.error("Environment variable 'ENDPOINT_SPEC' is in the wrong format")
-        raise Exception("Environment variable 'ENDPOINT_SPEC' is in the wrong format")
+    # verify if endpoint_spec is an array
+    if not isinstance(endpoint_spec, list):
+        logger.error("Environment variable 'ENDPOINT_SPEC' must be an array")
+        raise Exception("Environment variable 'ENDPOINT_SPEC' must be an array")
+    # check if every element in the array has the required  keys
+    for endpoint_spec_element in endpoint_spec:
+        if not ('EndpointConfigName' in endpoint_spec_element and 'EndpointName' in endpoint_spec_element):
+            logger.error("Environment variable 'ENDPOINT_SPEC' is in the wrong format")
+            raise Exception("Environment variable 'ENDPOINT_SPEC' is in the wrong format")
 
-    logger.info("Creating SageMaker Endpoint")
     sm_client = boto3.client('sagemaker')
-    response = sm_client.create_endpoint(
-        EndpointName=endpoint_spec['EndpointName'],
-        EndpointConfigName=endpoint_spec['EndpointConfigName']
-    )
+    anyFailed = False
+    for endpoint_spec_element in endpoint_spec:
+        logger.info("Creating SageMaker Endpoint: " + endpoint_spec_element['EndpointName'])
+        response = sm_client.create_endpoint(
+            EndpointName=endpoint_spec_element['EndpointName'],
+            EndpointConfigName=endpoint_spec_element['EndpointConfigName']
+        )
     
-    # Check the response
-    if response['ResponseMetadata']['HTTPStatusCode'] != 200:
-        logger.error("Error creating endpoint: " + str(response))
-        raise Exception("Error creating endpoint: " + str(response))
+        if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+            logger.error("Error creating endpoint: " + str(response))
+            anyFailed = True
+            # continue with the other endpoint creation
+            
+    if anyFailed:
+        raise Exception("Error creating endpoint")
         
     return {
         'statusCode': 200,
